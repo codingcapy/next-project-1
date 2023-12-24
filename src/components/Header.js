9c7a62780b942f761d5453968d7aa19307cee0ea
Paul <spkim0921@gmail.com>
2023-12-24 12:17:37 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="py-5 flex justify-between z-10 sticky top-0">
+            <div>
+                <Link href={"/"} className="px-5">CapyNext</Link>
+                <Link href={"/"} className="px-5">Home</Link>
+            </div>
+            <div>
+                <Link href={"/posts"} className="px-5">Posts</Link>
+            </div>
+            <div>
+                <Link href={"/users/login"} className="px-5">Login</Link>
+                <Link href={"/users/signup"} className="px-5">Sign up</Link>
+            </div>
+        </header>
+    )
+}
